68ee7398e4ff9e979e87f7d61e05d650dd6d725b
Ross Duquette <ross.duquette@gastronomous.ca>
2025-04-15 13:45:41 -0700

    Echo messages from 'test' to topic 'echo'

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include "mqtt/client.h"
 
+#include <vector>
+
 int main()
 {
     mqtt::client client("localhost", "example_client");
     client.connect();
-    const char* topic = "test";
-    const char* payload = "hello world";
-    const int qos = 1;
-    auto msg = mqtt::make_message(topic, payload);
-    msg->set_qos(qos);
-    client.publish(msg);
+
+    const std::vector<std::string> TOPICS = {
+        "test",
+    };
+    client.subscribe(TOPICS);
+
+    while (true) {
+        auto rx_msg = client.consume_message();
+        if (rx_msg && rx_msg->get_topic() == "test") {
+            const char* topic = "echo";
+            auto tx_msg = mqtt::make_message(topic, rx_msg->to_string());
+            tx_msg->set_qos(1);
+            client.publish(tx_msg);
+        }
+    }
     return 0;
 }
