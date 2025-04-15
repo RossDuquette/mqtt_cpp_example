72fbfdda058497b562d19b5ef07b2302a432d40f
Ross Duquette <ross.duquette@gastronomous.ca>
2025-04-15 13:37:14 -0700

    Simple publish example

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,12 @@
 int main()
 {
     mqtt::client client("localhost", "example_client");
+    client.connect();
+    const char* topic = "test";
+    const char* payload = "hello world";
+    const int qos = 1;
+    auto msg = mqtt::make_message(topic, payload);
+    msg->set_qos(qos);
+    client.publish(msg);
     return 0;
 }
